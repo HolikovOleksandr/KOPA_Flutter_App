8ef6de3d07dd0eb45c955ce40ad2441fd00e8702
Holikov_Oleksandr <sanyagolikov@gmail.com>
2022-09-04 17:50:25 +0300

    Get started draw home screen
    Create new branc "dev"

--- lib/resources/asset_pathes.dart
+++ lib/resources/asset_pathes.dart
@@ -1,6 +1,8 @@
 const String pathSmallTriangle = 'assets/images/small_triangle.png';
+const String pahtSimpsonMouth = 'assets/images/simpson_mouth.png';
 const String pathCurveButton = 'assets/images/curve_button.png';
 const String pathFacebookIcon = 'assets/images/facebook.svg';
 const String pathSplashScreen = 'assets/images/splash.png';
 const String pathGoogleIcon = 'assets/images/google.svg';
 const String pathPhoneIcon = 'assets/images/phone.svg';
+const String pathSoartIcon = 'assets/images/soart.svg';
--- lib/src/ui/home/home_screen.dart
+++ lib/src/ui/home/home_screen.dart
@@ -1,3 +1,7 @@
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:kopa/resources/asset_pathes.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kopa/resources/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,18 +15,48 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home'), centerTitle: true),
-      body: SafeArea(
-        child: Column(
-          children: const [
-            Center(
-              child: Icon(
-                Icons.home,
-                size: 200,
+      backgroundColor: colorBackground,
+      body: Column(
+        children: [
+          const SizedBox(
+            width: 25,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 24.0,
+                ),
+                child: GestureDetector(
+                  onTap: () => Fluttertoast.showToast(
+                    msg: 'TODO: Soart product cards',
+                  ),
+                  child: SvgPicture.asset(
+                    pathSoartIcon,
+                    fit: BoxFit.scaleDown,
+                    height: 20,
+                    width: 20,
+                  ),
+                ),
               ),
-            )
-          ],
-        ),
+              Image.asset(
+                pahtSimpsonMouth,
+              ),
+            ],
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: const [
+                Icon(
+                  Icons.favorite,
+                  color: Colors.white,
+                  size: 50,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
