f06fca621b69bd89c5658077e264a0c1dbb252c3
Holikov_Oleksandr <sanyagolikov@gmail.com>
2022-09-28 12:01:48 +0300

    Created base controller and class user model

---
+++ lib/src/core/models/user_moder.dart
@@ -0,0 +1,12 @@
+class User {
+  final String phone, location, name, surname, email;
+
+  User({
+    required this.phone,
+    required this.location,
+    required this.name,
+    required this.surname,
+    required this.email,
+  });
+  
+}
